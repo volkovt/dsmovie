e28d432c3cb50026f92b9fbb3f30980cbb53deeb
Diego Melo <diego_melo_1991@hotmail.com>
2022-01-16 00:35:35 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,21 @@
 import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination/index'
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
 import { BASE_URL } from 'utils/request';
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data)
-        })
+    const [pageNumber, setPageNumber] = useState(0);
 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPageNumber(data.number)
+            })
+    }, [])
 
     return <>
         <Pagination />
